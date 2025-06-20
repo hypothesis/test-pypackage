b4fa6f5366b847669b72359a5ab81692884d8dc9
Sean Hammond <seanh@users.noreply.github.com>
2025-06-20 10:13:44 +0100

    Lint

--- tests/functional/cli_test.py
+++ tests/functional/cli_test.py
@@ -3,9 +3,9 @@ from subprocess import run
 
 def test_help():
     """Test the test-pypackage --help command."""
-    run(["test-pypackage", "--help"], check=True)  # noqa: S603, S607
+    run(["test-pypackage", "--help"], check=True)  # noqa: S607
 
 
 def test_version():
     """Test the test-pypackage --version command."""
-    run(["test-pypackage", "--version"], check=True)  # noqa: S603, S607
+    run(["test-pypackage", "--version"], check=True)  # noqa: S607
